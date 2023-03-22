28032bc00f956c33129889dbf8857b2a15794933
YUAN TIANJIAN <stilrmy@gmail.com>
2023-03-22 14:59:31 +0900

    autoencoder+xLSINDY(not working)

---
+++ SinglePendulum_Train_v2.py
@@ -0,0 +1,278 @@
+import numpy as np
+import sys 
+
+from autoencoder_v2 import loading,encoder_forward,encoder_derivative
+from sympy import symbols, simplify, derive_by_array
+from scipy.integrate import solve_ivp
+from xLSINDy_sp import *
+from sympy.physics.mechanics import *
+from sympy import *
+import sympy
+import torch
+import sys
+import HLsearch as HL
+import example_pendulum
+import time
+
+
+
+
+
+sys.path.append(r'../../../HLsearch/')
+device = 'cuda:0'
+data = example_pendulum.get_pendulum_data(40)
+
+
+
+#Saving Directory
+rootdir = "../Single Pendulum/Data/"
+
+num_sample = 100
+training = True
+save = False
+noiselevel = 2e-2
+file_route = R'C:\Users\87106\OneDrive\sindy\progress'
+load_date = '3-6'
+load_ver = 1
+widths = [1024,512,128]
+params={}
+params['ver'] = 1
+params['date'] = '3-7'
+params['widths'] = widths
+params['activation'] = 'sigmoid'
+params = loading(params,load_date,load_ver,file_route,device)
+image = torch.from_numpy(data['x']).to(torch.float32).to(device)
+image_t = torch.from_numpy(data['dx']).to(torch.float32).to(device)
+image_tt = torch.from_numpy(data['ddx']).to(torch.float32).to(device)
+x = encoder_forward(image,params)
+x = x.cpu().detach().numpy()
+dx,ddx = encoder_derivative(image,image_t,image_tt,params)
+dx = dx.cpu().detach().numpy()
+ddx = ddx.cpu().detach().numpy()
+X = []
+Xdot = []
+for i in range(len(x)):
+    temp_list = [float(x[i]),float(dx[i])]
+    print(temp_list)
+    X.append(temp_list)
+    temp_list = [float(dx[i]),float(ddx[i])]
+    Xdot.append(temp_list)
+X = np.vstack(X)
+print(X.shape)
+Xdot = np.vstack(Xdot)
+print(X.shape)
+
+
+
+
+
+#adding noise
+"""
+mu, sigma = 0, noiselevel
+noise = np.random.normal(mu, sigma, X.shape[0])
+for i in range(X.shape[1]):
+    X[:,i] = X[:,i]+noise
+    Xdot[:,i] = Xdot[:,i]+noise
+"""
+states_dim = 2
+states = ()
+states_dot = ()
+for i in range(states_dim):
+    if(i<states_dim//2):
+        states = states + (symbols('x{}'.format(i)),)
+        states_dot = states_dot + (symbols('x{}_t'.format(i)),)
+    else:
+        states = states + (symbols('x{}_t'.format(i-states_dim//2)),)
+        states_dot = states_dot + (symbols('x{}_tt'.format(i-states_dim//2)),)
+print('states are:',states)
+print('states derivatives are: ', states_dot)
+
+
+#Turn from sympy to str
+states_sym = states
+states_dot_sym = states_dot
+states = list(str(descr) for descr in states)
+states_dot = list(str(descr) for descr in states_dot)
+
+
+#build function expression for the library in str
+expr= HL.buildFunctionExpressions(2,states_dim,states,use_sine=True)
+#expr=['x0', 'x0_t', 'sin(x0)', 'cos(x0)', 'x0**2', 'x0*x0_t', 'x0_t**2', 'x0*sin(x0)', 'x0_t*sin(x0)', 'sin(x0)**2', 'x0*cos(x0)', 'x0_t*cos(x0)', 'sin(x0)*cos(x0)', 'cos(x0)**2']
+"a list of candidate function"
+print(expr)
+expr.pop(5)
+"?"
+
+device = 'cuda:0'
+
+
+Zeta, Eta, Delta = LagrangianLibraryTensor(X,Xdot,expr,states,states_dot,scaling=True)
+Eta = Eta.to(device)
+Zeta = Zeta.to(device)
+Delta = Delta.to(device)
+
+
+mask = torch.ones(len(expr),device=device)
+xi_L = torch.ones(len(expr), device=device).data.uniform_(-10,10)
+prevxi_L = xi_L.clone().detach()
+
+
+def loss(pred, targ):
+    loss = torch.mean((pred - targ)**2) 
+    return loss 
+
+
+def clip(w, alpha):
+    clipped = torch.minimum(w,alpha)
+    clipped = torch.maximum(clipped,-alpha)
+    return clipped
+
+def proxL1norm(w_hat, alpha):
+    if(torch.is_tensor(alpha)==False):
+        alpha = torch.tensor(alpha)
+    w = w_hat - clip(w_hat,alpha)
+    return w
+
+
+def training_loop(coef, prevcoef, Zeta, Eta, Delta,xdot, bs, lr, lam):
+    loss_list = []
+    tl = xdot.shape[0]
+    n = xdot.shape[1]
+    momentum = True
+    if(torch.is_tensor(xdot)==False):
+        xdot = torch.from_numpy(xdot).to(device).float()
+
+    v = coef.clone().detach().to(device).requires_grad_(True)
+    prev = prevcoef.clone().detach().to(device).requires_grad_(True)
+
+    for i in range(tl//bs):
+
+        #computing acceleration with momentum
+        #if (momentum == True):
+            #vhat = (v + ((i - 1) / (i + 2)) * (v - prev)).clone().detach().requires_grad_(True)
+       # else:
+        vhat = v.requires_grad_(True).clone().detach().requires_grad_(True)
+
+        prev = v
+
+        #Computing loss
+        zeta = Zeta[:,i*bs:(i+1)*bs]
+        eta = Eta[:,i*bs:(i+1)*bs]
+        delta = Delta[:,i*bs:(i+1)*bs]
+        x_t = xdot[i*bs:(i+1)*bs,:]
+
+        #forward
+        q_tt_pred = lagrangianforward(vhat,zeta,eta,delta,x_t,device)
+        q_tt_true = xdot[i*bs:(i+1)*bs,n//2:].T
+
+
+
+        lossval = loss(q_tt_pred, q_tt_true)
+        lossval.requires_grad_(True)
+        #Backpropagation
+
+        lossval.backward()
+
+        with torch.no_grad():
+            v = vhat - lr * vhat.grad
+            v = proxL1norm(v, lr * lam)
+
+            # Manually zero the gradients after updating weights
+            vhat.grad = None
+
+
+        loss_list.append(lossval.item())
+    print("Average loss : " , torch.tensor(loss_list).mean().item())
+    return v, prev, torch.tensor(loss_list).mean().item()
+
+
+Epoch = 100
+i = 1
+lr = 1e-3
+lam = 0.1
+temp = 1000
+while(i<=Epoch):
+    print("\n")
+    print("Stage 1")
+    print("Epoch "+str(i) + "/" + str(Epoch))
+    print("Learning rate : ", lr)
+    xi_L , prevxi_L, lossitem= training_loop(xi_L,prevxi_L,Zeta,Eta,Delta,Xdot,128,lr=lr,lam=lam)
+    if(temp <=5e-3):
+        break
+    if(temp <=1e-1):
+        lr = 1e-5
+    temp = lossitem
+    i+=1
+
+
+## Thresholding small indices ##
+threshold = 1e-2
+surv_index = ((torch.abs(xi_L) >= threshold)).nonzero(as_tuple=True)[0].detach().cpu().numpy()
+expr = np.array(expr)[surv_index].tolist()
+
+xi_L =xi_L[surv_index].clone().detach().requires_grad_(True)
+prevxi_L = xi_L.clone().detach()
+mask = torch.ones(len(expr),device=device)
+
+## obtaining analytical model
+xi_Lcpu = np.around(xi_L.detach().cpu().numpy(),decimals=2)
+L = HL.generateExpression(xi_Lcpu,expr,threshold=1e-2)
+print(simplify(L))
+
+
+
+## Next round Selection ##
+for stage in range(100):
+    
+    #Redefine computation after thresholding
+    Zeta, Eta, Delta = LagrangianLibraryTensor(X,Xdot,expr,states,states_dot,scaling=False)
+    Eta = Eta.to(device)
+    Zeta = Zeta.to(device)
+    Delta = Delta.to(device)
+
+    #Training
+    Epoch = 100
+    i = 1
+    lr = 1e-4
+    if(stage==1):
+        lam = 0
+    else:
+        lam = 0.1
+    temp = 1000
+    while(i<=Epoch):
+        print("\n")
+        print("Stage " + str(stage+2))
+        print("Epoch "+str(i) + "/" + str(Epoch))
+        print("Learning rate : ", lr)
+        xi_L , prevxi_L, lossitem= training_loop(xi_L,prevxi_L,Zeta,Eta,Delta,Xdot,128,lr=lr,lam=lam)
+        temp = lossitem
+        if(temp <=1e-6):
+            break
+        i+=1
+    
+    ## Thresholding small indices ##
+    threshold = 1e-1
+    surv_index = ((torch.abs(xi_L) >= threshold)).nonzero(as_tuple=True)[0].detach().cpu().numpy()
+    expr = np.array(expr)[surv_index].tolist()
+
+    xi_L =xi_L[surv_index].clone().detach().requires_grad_(True)
+    prevxi_L = xi_L.clone().detach()
+    mask = torch.ones(len(expr),device=device)
+
+    ## obtaining analytical model
+    xi_Lcpu = np.around(xi_L.detach().cpu().numpy(),decimals=3)
+    L = HL.generateExpression(xi_Lcpu,expr,threshold=1e-2)
+    print("Result stage " + str(stage+2) + ":" , simplify(L))
+
+
+if(save==True):
+    #Saving Equation in string
+    text_file = open(rootdir + "lagrangian_" + str(noiselevel)+ "_noise.txt", "w")
+    text_file.write(L)
+    text_file.close()
+
+
+# %%
+
+
+
---
+++ autoencoder_v3.py
@@ -0,0 +1,302 @@
+def __clear_env():
+    for key in globals().keys():
+        if not key.startswith("__"):# 排除系统内建函数
+            globals().pop(key)
+__clear_env
+import example_pendulum
+import torch; torch.manual_seed(0)
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils
+import torch.distributions
+#import torchvision
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import pickle
+from torch.autograd import Variable
+device = 'cuda:0'
+data = example_pendulum.get_pendulum_data(40)
+val_data = example_pendulum.get_pendulum_data(10)
+
+
+def autoencoder(input_dim,latent_dim,widths,device):
+    #generate the parameters of the autoencoder
+    encoder_weights,encoder_biases = build_network_layers(input_dim,latent_dim,widths,device)
+    decoder_weights, decoder_biases = build_network_layers(latent_dim, input_dim, widths[::-1],device)
+    return encoder_weights,encoder_biases,decoder_weights,decoder_biases
+
+
+def build_network_layers(input_dim,output_dim,widths,device):
+    #universal function for building network
+    weights = []
+    biases = []
+    last_width = input_dim
+    #middle layers
+    for i,n_units in enumerate(widths):
+        w = torch.Tensor(last_width,n_units,).to(device)
+        nn.init.xavier_uniform_(w, gain=1.0)
+        w = Variable(w, requires_grad=True)
+        b = torch.Tensor(n_units).to(device)
+        nn.init.constant_(b, 0.0)
+        b = Variable(b, requires_grad=True)
+        last_width = n_units
+        weights.append(w)
+        biases.append(b)
+    #latent layer
+    w = torch.Tensor(last_width,output_dim).to(device)
+    nn.init.xavier_uniform_(w, gain=1.0)
+    w = Variable(w,requires_grad=True)
+    b = torch.Tensor(output_dim).to(device)
+    nn.init.constant_(b, 0.0)
+    b = Variable(b, requires_grad=True)
+    weights.append(w)
+    biases.append(b)
+    return weights,biases
+
+def autoencoder_forward(input,params):
+    #pass through the autoencoder
+    for i,weights in enumerate(params['encoder_weights']):
+        input = torch.matmul(input,weights)+params['encoder_biases'][i]
+        if params['activation'] == 'sigmoid' and i <len(params['encoder_weights'])-1:
+            input = torch.sigmoid(input)
+    for i,weights in enumerate(params['decoder_weights']):
+        input = torch.matmul(input,weights)+params['decoder_biases'][i]
+        if params['activation'] == 'sigmoid'and i <len(params['decoder_weights'])-1:
+            input = torch.sigmoid(input)
+    return input
+
+def encoder_forward(input,params):
+    for i,weights in enumerate(params['encoder_weights']):
+        input = torch.matmul(input,weights)+params['encoder_biases'][i]
+        if params['activation'] == 'sigmoid' and i <len(params['encoder_weights'])-1:
+            input = torch.sigmoid(input)
+    return input
+
+def decoder_forward(input,params):
+    for i,weights in enumerate(params['decoder_weights']):
+        input = torch.matmul(input,weights)+params['decoder_biases'][i]
+        if params['activation'] == 'sigmoid' and i <len(params['decoder_weights'])-1:
+            input = torch.sigmoid(input)
+    return input
+
+def encoder_derivative(image,image_t,image_tt,params):
+    #calculate the time/second time derivative of the latent(s) using the autoencoder
+    dz = image_t
+    ddz = image_tt
+    input = image
+    weights = params['encoder_weights']
+    biases = params['encoder_biases']
+    if params['activation'] == 'sigmoid':
+        for i in range(len(weights)-1):
+            input = torch.matmul(input,weights[i])+biases[i]
+            input = torch.sigmoid(input)
+            dz_prev = torch.matmul(dz,weights[i])
+            sigmoid_derivative = torch.multiply(input,1-input)
+            sigmoid_derivative2 = torch.multiply(sigmoid_derivative,1-2*input)
+            dz = torch.multiply(sigmoid_derivative,dz_prev)
+            ddz = torch.multiply(sigmoid_derivative2,torch.square(dz_prev))\
+                  + torch.multiply(sigmoid_derivative,torch.matmul(ddz,weights[i]))
+        dz = torch.matmul(dz,weights[-1])
+        ddz = torch.matmul(ddz,weights[-1])
+    return dz,ddz
+
+def decoder_derivative(z,dz,ddz,params):
+    #calculate the time/second time derivative of the latent(s) using the autoencoder
+    input = z
+    weights = params['decoder_weights']
+    biases = params['decoder_biases']
+    if params['activation'] == 'sigmoid':
+        for i in range(len(weights)-1):
+            input = torch.matmul(input,weights[i])+biases[i]
+            input = torch.sigmoid(input)
+            dz_prev = torch.matmul(dz,weights[i])
+            sigmoid_derivative = torch.multiply(input,1-input)
+            sigmoid_derivative2 = torch.multiply(sigmoid_derivative,1-2*input)
+            dz = torch.multiply(sigmoid_derivative,dz_prev)
+            #print(sigmoid_derivative.shape)
+            ddz = torch.multiply(sigmoid_derivative2,torch.square(dz_prev))\
+                  + torch.multiply(sigmoid_derivative,torch.matmul(ddz,weights[i]))
+        dz = torch.matmul(dz,weights[-1])
+        ddz = torch.matmul(ddz,weights[-1])
+    return dz,ddz
+
+def training(data,params,device):
+    opt = torch.optim.Adam(generate_parameter(params), lr=params['learning_rate'])
+    total_loss = 0
+    total_loss_z = 0
+    for j in range(params['epoch_size']//params['batch_size']):
+        batch_idxs = np.arange(j * params['batch_size'], (j + 1) * params['batch_size'])
+        z = torch.from_numpy(data['z'][batch_idxs]).to(torch.float32).to(device)
+        x = torch.from_numpy(data['x'][batch_idxs]).to(torch.float32).to(device)
+        dx = torch.from_numpy(data['dx'][batch_idxs]).to(torch.float32).to(device)
+        ddx = torch.from_numpy(data['ddx'][batch_idxs]).to(torch.float32).to(device)
+        x_predict = autoencoder_forward(x,params)
+        z_predict = encoder_forward(x,params)
+        dz_predict,ddz_predict = encoder_derivative(x,dx,ddx,params)
+        dx_predict,ddx_predict = decoder_derivative(z_predict,dz_predict,ddz_predict,params)
+        loss_x = torch.mean((x - x_predict)**2)
+        loss_dx = torch.mean((x - dx_predict) ** 2)
+        loss_ddx = torch.mean((x - ddx_predict) ** 2)
+        loss_z = torch.mean((z - z_predict)**2)
+        loss = params['loss_weight_x'] * loss_x \
+               + params['loss_weight_dx'] * loss_dx \
+               + params['loss_weight_ddx'] * loss_ddx
+        total_loss += loss
+        total_loss_z += loss_z
+        loss.backward()
+        opt.step()
+        opt.zero_grad()
+    avg_loss = total_loss/(params['epoch_size']//params['batch_size'])
+    avg_loss_z = total_loss_z/(params['epoch_size']//params['batch_size'])
+    return avg_loss,avg_loss_z
+
+def generate_parameter(params):
+    encoder_weights = params['encoder_weights']
+    encoder_biases = params['encoder_biases']
+    params_temp = []
+    for i in range(len(params['widths'])):
+        params_temp.append(encoder_weights[i])
+        params_temp.append(encoder_biases[i])
+    decoder_weights = params['decoder_weights']
+    decoder_biases = params['decoder_biases']
+    for i in range(len(params['widths'])):
+        params_temp.append(decoder_weights[i])
+        params_temp.append(decoder_biases[i])
+    return params_temp
+
+def generate_vhat(vhat):
+    vhat = tuple(vhat)
+    yield vhat
+
+def validation(data,params,device):
+    total_loss = 0
+    total_loss_z = 0
+    params['epoch_size'] = val_data["x"].shape[0]
+    for j in range(params['epoch_size']//params['batch_size']):
+        batch_idxs = np.arange(j * params['batch_size'], (j + 1) * params['batch_size'])
+        z = torch.from_numpy(data['z'][batch_idxs]).to(torch.float32).to(device)
+        x = torch.from_numpy(data['x'][batch_idxs]).to(torch.float32).to(device)
+        dx = torch.from_numpy(data['dx'][batch_idxs]).to(torch.float32).to(device)
+        ddx = torch.from_numpy(data['ddx'][batch_idxs]).to(torch.float32).to(device)
+        x_predict = autoencoder_forward(x,params)
+        z_predict = encoder_forward(x,params)
+        dz_predict,ddz_predict = encoder_derivative(x,dx,ddx,params)
+        dx_predict,ddx_predict = decoder_derivative(z_predict,dz_predict,ddz_predict,params)
+        loss_x = torch.mean((x - x_predict)**2)
+        loss_dx = torch.mean((x - dx_predict) ** 2)
+        loss_ddx = torch.mean((x - ddx_predict) ** 2)
+        loss_z = torch.mean((z - z_predict)**2)
+        loss = params['loss_weight_x'] * loss_x \
+               + params['loss_weight_dx'] * loss_dx \
+               + params['loss_weight_ddx'] * loss_ddx
+        total_loss += loss
+        total_loss_z += loss_z
+        print('batch_{} --- image loss: {} --- z loss: {}'.format(j,loss,loss_z))
+    avg_loss = total_loss/(params['epoch_size']//params['batch_size'])
+    avg_loss_z = total_loss_z/(params['epoch_size']//params['batch_size'])
+    return avg_loss,avg_loss_z
+
+def plotting(loss_history,loss_z_history):
+    fig, a = plt.subplots(2, 1)
+    a[0].plot(loss_history)
+    a[0].set_title("loss")
+    a[1].plot(loss_z_history)
+    a[1].set_title("z_loss")
+    plt.subplots_adjust(hspace=1)
+    plt.show()
+    return
+
+def saving(params,coef,expr,L):
+    params_names = [params['encoder_weights'],params['encoder_biases'],params['decoder_weights'],params['decoder_biases']]
+    params_names_str = ['encoder_weights','encoder_biases','decoder_weights','decoder_biases']
+    for j,param_set in enumerate(params_names):
+        for i,elements in enumerate(param_set):
+            np.save(r"E:\OneDrive\sindy\progress\{}\{}\autoencoder_{}_layer{}.npy".format(params['date'],params['ver'],params_names_str[j],i),
+                   elements.clone().detach().cpu().numpy())
+    np.save(r"E:\OneDrive\sindy\progress\{}\{}\coef.npy".format(params['date'],params['ver']),coef.clone().detach().cpu().numpy())
+    np.save(r"E:\OneDrive\sindy\progress\{}\{}\expr.npy".format(params['date'], params['ver']),
+            expr)
+    sub_params = params
+    del sub_params['encoder_weights']
+    del sub_params['encoder_biases']
+    del sub_params['decoder_weights']
+    del sub_params['decoder_biases']
+    with open(r"E:\OneDrive\sindy\progress\{}\{}\params.txt".format(params['date'],params['ver']),'w') as file:
+        file.write(str(sub_params))
+        file.write('\r\t')
+        file.write(L)
+        file.write('\r\t')
+        file.close()
+    print('params saved')
+    return
+
+def loading_coef(params,date,load_number,file_route,device):
+    print('loading params')
+    params_names_str = ['encoder_weights', 'encoder_biases', 'decoder_weights', 'decoder_biases']
+    for param_name in params_names_str:
+        params['{}'.format(param_name)] = []
+        for i in range(len(params['widths'])+1):
+            temp_loader = np.load(r'{}\{}\{}\autoencoder_{}_layer{}.npy'.format(file_route,date,load_number,param_name,i))
+            temp_loader = torch.tensor(temp_loader).to(device)
+            temp_loader = Variable(temp_loader, requires_grad=True)
+            print(temp_loader.shape)
+            params['{}'.format(param_name)].append(temp_loader)
+    coef = np.load(r'{}\{}\{}\coef.npy'.format(file_route,date,load_number))
+    coef = torch.Tensor(coef).to(device)
+    coef = Variable(coef,requires_grad=True)
+    expr = np.load(r'{}\{}\{}\expr.npy'.format(file_route,date,load_number))
+    return params,coef,expr
+
+# initialization
+# file_route = R'C:\Users\87106\OneDrive\sindy\progress'
+"""
+save_params = True
+load_params = True
+
+load_date = '3-7'
+load_ver = 2
+widths = [1024,512,128]
+params={}
+params['ver'] = "3"
+params['accumulate_epochs'] = 6000
+params['date'] = '3-7'
+params['widths'] = widths
+
+if load_params == True:
+    params = loading(params,load_date,load_ver,file_route,device)
+else:
+    encoder_weights,encoder_biases,decoder_weights,decoder_biases = autoencoder(2601,1,widths,device)
+    params['encoder_weights'] = encoder_weights
+    params['encoder_biases'] = encoder_biases
+    params['decoder_weights'] = decoder_weights
+    params['decoder_biases'] = decoder_biases
+
+params['activation'] = 'sigmoid'
+params['max_epochs'] = 2000
+params['epoch_size'] = data["x"].shape[0]
+params['batch_size'] = 500
+params['learning_rate'] = 0
+params['learning_rate_stage1'] = 1e-3
+params['learning_rate_stage2'] = 1e-3
+params['loss_weight_x'] = 1
+params['loss_weight_dx'] = 1
+params['loss_weight_ddx'] = 1
+params['learning_rate_switching_point'] = 700
+loss_history = []
+loss_z_history = []
+params['learning_rate'] = params['learning_rate_stage1']
+
+for epochs in range(params['max_epochs']):
+    if epochs >= params['learning_rate_switching_point']:
+        params['learning_rate'] = params['learning_rate_stage2']
+    loss,loss_z = training(data,params,device)
+    loss_history.append(loss.clone().detach().cpu().numpy())
+    loss_z_history.append(loss_z.clone().detach().cpu().numpy())
+    print('epoch: {} === loss: {}\t=== loss_z: {}'.format(epochs,loss,loss_z))
+
+validation(val_data,params,device)
+if save_params == True:
+    saving(params)
+plotting(loss_history,loss_z_history)
+"""
